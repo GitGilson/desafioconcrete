e244d0b3ae9b41cd9851e3b2bdca54b4e4186233
Gilson Lima <gilsonlima1981@gmail.com>
2019-06-25 20:36:45 -0300

    ajuste gitignore

---
+++ build/precache-manifest.495d9c2716c130603e06f4b47f6015c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da6645706a3b49a96bdf8119cbbf4589",
+    "url": "/GitGilson/desafioconcrete/index.html"
+  },
+  {
+    "revision": "9a16d4f62f2b94d671a9",
+    "url": "/GitGilson/desafioconcrete/static/css/main.70d79b00.chunk.css"
+  },
+  {
+    "revision": "177ef4f15dce1236c991",
+    "url": "/GitGilson/desafioconcrete/static/js/2.8fc621fc.chunk.js"
+  },
+  {
+    "revision": "9a16d4f62f2b94d671a9",
+    "url": "/GitGilson/desafioconcrete/static/js/main.a6cb4dd3.chunk.js"
+  },
+  {
+    "revision": "621fcdb182f15a7b4395",
+    "url": "/GitGilson/desafioconcrete/static/js/runtime~main.d79a1b42.js"
+  }
+]);
